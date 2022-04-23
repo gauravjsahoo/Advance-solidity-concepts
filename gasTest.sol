f3cd6c8c043081236aab7e10deda42402daeae79
Gaurav Sahoo <104127660+gauravjsahoo@users.noreply.github.com>
2022-04-23 17:46:34 +0530

    Create gasTest.sol

---
+++ gasTest.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.7;
+
+contract GasTest{
+
+    function gasTest(uint[] calldata vartemp) external returns(uint)
+    {
+
+    }
+
+    function gasTest2(uint[] memory vartemp) public returns(uint)
+    {
+
+    }
+
+}
